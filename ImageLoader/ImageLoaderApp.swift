5a571481b8bed5de397348bf38ac788d7e92f6af
Jem1s <Jem1s@bk.ru>
2024-10-04 13:48:05 +0500

    Initial Commit

---
+++ ImageLoader/ImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageLoaderApp.swift
+//  ImageLoader
+//
+//  Created by Кирилл Уваров on 04.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
